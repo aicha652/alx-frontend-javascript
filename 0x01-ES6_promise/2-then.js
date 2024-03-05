92eb7ad3e342a5a90065146655d082684dce45a7
aicha652 <aichaazlf652@gmail.com>
2024-03-05 15:49:25 +0000

    fix task 2

--- 0x01-ES6_promise/2-then.js
+++ 0x01-ES6_promise/2-then.js
@@ -1,8 +1,12 @@
 /* eslint-disable no-unused-vars */
 export default function handleResponseFromAPI(promise) {
   return new Promise((resolve, reject) => {
-    resolve({ status: 200, body: 'success' });
-    reject(new Error());
-    console.log('Got a response from the API');
+    try {
+	  resolve({ status: 200, body: 'success' });
+	  reject();
+	  console.log('Got a response from the API');
+    } catch (error){
+	    throw(error);
+    }
   });
 }
