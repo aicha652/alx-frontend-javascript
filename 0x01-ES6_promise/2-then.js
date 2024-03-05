e7f42ac81893563c8e46a03b335e7a4753c74586
aicha652 <aichaazlf652@gmail.com>
2024-03-05 19:54:42 +0000

    fix lask check

--- 0x01-ES6_promise/2-then.js
+++ 0x01-ES6_promise/2-then.js
@@ -3,11 +3,13 @@ export default function handleResponseFromAPI(promise) {
   return new Promise((resolve, reject) => {
     promise
       .then(() => {
-        resolve({ status: 200, body: 'success' });
-        console.log('Got a response from the API');
+	({ status: 200, body: 'success' });
       })
       .catch(() => {
-        console.error('Got a response from the API');
+	new Error();
+      })
+      .finally(() => {
+	console.log('Got a response from the API');
       });
   });
 }
