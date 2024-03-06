de870ed6febe143ffc85c71ad2829189945d2ce7
aicha652 <aichaazlf652@gmail.com>
2024-03-06 15:12:01 +0000

    fix

--- 0x01-ES6_promise/6-final-user.js
+++ 0x01-ES6_promise/6-final-user.js
@@ -2,8 +2,9 @@ import signUpUser from './4-user-promise';
 import uploadPhoto from './5-photo-reject';
 
 export default function handleProfileSignup(firstName, lastName, fileName) {
-  Promise.allSettled([
+  return Promise.allSettled([
     signUpUser(firstName, lastName),
     uploadPhoto(fileName)]).then((values) => {
-      console.log(values)});
+    console.log(values);
+  });
 }
