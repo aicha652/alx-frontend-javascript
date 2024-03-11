f2350b01303a1568650c2a0017fc135fdaea4f11
aicha652 <aichaazlf652@gmail.com>
2024-03-11 17:22:40 +0000

    task 4

--- 0x03-ES6_data_manipulation/4-update_grade_by_city.js
+++ 0x03-ES6_data_manipulation/4-update_grade_by_city.js
@@ -1,10 +1,12 @@
 export default function updateStudentGradeByCity(StudentList, city, newGrades) {
   const students = StudentList.filter((student) => student.location === city);
-  
-  return students.map((student) => {const grade = newGrades.find(grade => grade.studentId === student.id)
-  student.grade = grade
-  ? grade.grade
-  : 'N/A'
-  return student;
+
+  return students.map((student) => {
+    const grade = newGrades.find((grade) => grade.studentId === student.id);
+    const VarStudent = { ...student };
+    VarStudent.grade = grade
+      ? grade.grade
+      : 'N/A';
+    return VarStudent;
   });
 }
