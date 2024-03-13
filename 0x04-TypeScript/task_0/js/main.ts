a29f4e2d19dbf341c268301d6091d1aac42935c6
aicha652 <aichaazlf652@gmail.com>
2024-03-13 13:35:35 +0000

    first commit

---
+++ 0x04-TypeScript/task_0/js/main.ts
@@ -0,0 +1,10 @@
+interface Student {
+  firstName: string;
+  lastName: string;
+  age: number;
+  location: string;
+}
+const student1: Student = { firstName: "ahmed", lastName: "ahmedLastName", age: 40, location: "casa" };
+const student2: Student = { firstName: "adam", lastName: "adamLastName", age: 18, location: "rabat" };
+
+studentsList = [student1, student2];
